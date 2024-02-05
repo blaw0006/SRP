13d2b1927b99325b13f511a12b8cf564d6abcdf3
Owen Brooks <owen.h.brooks@gmail.com>
2024-02-05 12:35:48 +1100

    Changed record_wavmp3 to only use one callback function with callback args (version before did not work)

--- src/audio_writer/record_wavmp3.py
+++ src/audio_writer/record_wavmp3.py
@@ -19,20 +19,20 @@ class record_wavmp3():
         rospy.init_node('record_wav', anonymous=True) # avoid duplicate node names with anonymous=True
         
         # Mic1
-        rospy.Subscriber(topic1, inputMsg, self.audio_callback1)
         self.data1 = AudioSegment.silent(duration=0) # empty audiosegment that will be appended to each callback
         self.wav_file_to_write1 = wav_file_to_write[0]
         self.mp3_file_to_write1 = mp3_file_to_write[0]
+        rospy.Subscriber(topic1, inputMsg, self.audio_callback, callback_args=1)
         #self.lock = Lock() # create threadlock for thread synchronisation
         
         # Mic 2 
-        rospy.Subscriber(topic2, inputMsg, self.audio_callback2)
         self.data2 = AudioSegment.silent(duration=0) # empty audiosegment that will be appended to each callback
         self.wav_file_to_write2 = wav_file_to_write[1]
         self.mp3_file_to_write2 = mp3_file_to_write[1]
+        rospy.Subscriber(topic2, inputMsg, self.audio_callback, callback_args=2)
         
         
-    def audio_callback1(self, data):
+    def audio_callback(self, data, args):
         '''
         Callback function converts raw mp3 -> AudioSegment -> wav -> np_array
         
@@ -49,36 +49,20 @@ class record_wavmp3():
             frame_rate=16000,
             channels=1
         )
-    
-        self.data1 = self.data1 + audio_segment # append the new data to self.data
+
+        print(args)
         
-    def audio_callback2(self, data):
-        '''
-        Callback function converts raw mp3 -> AudioSegment -> wav -> np_array
-        
-        AudioSegment accepts either file path to mp3 file or raw mp3 data (in the form of bytestrings) as input
-        Since data.data accesses the raw audio data as a bytestring, no further processing is needed to convert
-        it to a bytestring (np.array(input) and np_array.tobytes() do not have to be used).
-        '''
-        audio_samples = data.data # need the .data field to access the mp3 data (hexadecimal bytestring of the form '\xff\xf3\xb8\xc4\r}d\x15\xd8')
-    
-        # create AudioSegment object from the raw mp3 data
-        audio_segment = AudioSegment(
-            audio_samples,  
-            sample_width=2, # sample width is number of bytes used to represent one element-> S16LE means 16 bit byte width
-            frame_rate=16000,
-            channels=1
-        )
-    
-        self.data2 = self.data2 + audio_segment # append the new data to self.data
-        
-       
+        if args == 1:
+            self.data1 = self.data1 + audio_segment # append the new data to self.data
+        elif args == 2:
+            self.data2 = self.data2 + audio_segment
     
     def shutdown_callback(self):
         '''
         Note that all AudioSegment.from_file methods return an AudioSegment object.
         '''
-        
+        print(self.data1)
+        print(self.data2)
         # convert audiosegment to wav/mp3 and export
         # Mic 1
         self.data1.export(self.wav_file_to_write1, format="wav") # save wav file first
