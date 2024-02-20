e398ea10812b6dd6f7c60931b36eaadd58645085
acrv <blaw0006@student.monash.edu>
2024-02-20 13:05:40 +1100

    Adding documentation and trained some models (saved locally)

---
+++ documentation.md
@@ -0,0 +1,17 @@
+
+upload the trained models??? Or just leave locally, tell them where it is and how to train the model themselves.
+
+Recording
+- check they are connected with arecord -l
+- using audio_capture
+- using record.launch and record_node (check the labels and stuff)
+
+Experimental setup
+
+Training models
+- need to download PANNS model from here: ____ separately, due to the size of the file
+- using runme.sh
+- changing the output layer to fit your model
+- using inference.sh
+
+Note: trained models are not on the github due to filesize limitations, they are on the UR5 PC
--- panns_transfer_to_gtzan/runme.sh
+++ panns_transfer_to_gtzan/runme.sh
@@ -2,11 +2,12 @@
 #DATASET_DIR="/home/caitlin/blaw_ws/src/ur5_control/src/SRP/src/wav_files" # assumes that the directory is setup with the labels (contains a 'collision' and 'no collision' folder)
 #DATASET_DIR="/home/acrv/blaw_ws/src/ur5_control/src/SRP/src/wav_files"
 #DATASET_DIR="/home/acrv/blaw_ws/ur5_control/src/SRP/src/mp3_test"
-DATASET_DIR="/home/acrv/blaw_ws/mp3_data"
+#DATASET_DIR="/home/acrv/blaw_ws/mp3_data"
+#DATASET_DIR="/home/acrv/blaw_ws/ur5_control/src/SRP/src/1_mic_data/mp3_data"
+DATASET_DIR="/home/acrv/blaw_ws/ur5_control/src/SRP/src/4_mic_data/mp3_data"
 
-#WORKSPACE="/home/caitlin/blaw_ws/src/panns_transfer_to_gtzan"
-#WORKSPACE="/home/acrv/blaw_ws/src/ur5_control/src/SRP/panns_transfer_to_gtzan"
-WORKSPACE="/home/acrv/blaw_ws/workspace" # store checkpoints and logs outside git repo since they are very large
+#WORKSPACE="/home/acrv/blaw_ws/workspace_1mic" # store checkpoints and logs outside git repo since they are very large
+WORKSPACE="/home/acrv/blaw_ws/workspace_4mic" 
 
 python3 ../panns_transfer_to_gtzan/utils/features.py pack_audio_files_to_hdf5 --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE 
 
@@ -16,7 +17,7 @@ PRETRAINED_CHECKPOINT_PATH="/home/acrv/blaw_ws/pretrained/Cnn14_16k_mAP=0.438.pt
 
 
 #CUDA_VISIBLE_DEVICES=3 python3 pytorch/main.py train --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --holdout_fold=1 --model_type="Transfer_Cnn14" --pretrained_checkpoint_path=$PRETRAINED_CHECKPOINT_PATH --loss_type=clip_nll --augmentation='mixup' --learning_rate=1e-4 --batch_size=32 --resume_iteration=0 --stop_iteration=10000 --cuda
-python3 pytorch/main.py train --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --holdout_fold=2 --model_type="Transfer_Cnn14_16k" --pretrained_checkpoint_path=$PRETRAINED_CHECKPOINT_PATH --loss_type=clip_nll --augmentation='mixup' --learning_rate=1e-4 --batch_size=2 --resume_iteration=0 --stop_iteration=10000 --cuda
+python3 pytorch/main.py train --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --holdout_fold=4 --model_type="Transfer_Cnn14_16k" --pretrained_checkpoint_path=$PRETRAINED_CHECKPOINT_PATH --loss_type=clip_nll --augmentation='mixup' --learning_rate=1e-4 --batch_size=8 --resume_iteration=0 --stop_iteration=400 --cuda
 
 
 #####
